f7c73663d96086cd01601693986ceeb5b9a83279
naszam <nazzareno@nazzarenomassari.com>
2020-06-16 21:54:42 +0100

    update to solc v0.6.10

--- contracts/Badges/Badges.sol
+++ contracts/Badges/Badges.sol
@@ -1,5 +1,5 @@
 /// SPDX-License-Identifier: BSD-3-Clause
-pragma solidity 0.6.9;
+pragma solidity 0.6.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
--- contracts/Migrations/Migrations.sol
+++ contracts/Migrations/Migrations.sol
@@ -1,5 +1,5 @@
 /// SPDX-License-Identifier: BSD-3-Clause
-pragma solidity 0.6.9;
+pragma solidity 0.6.10;
 
 contract Migrations {
   address public owner;
